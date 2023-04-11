33b2802da4cbd02f10ea6bc9ad46bc69767c145a
Qubav <124883831+Qubav@users.noreply.github.com>
2023-04-11 10:57:21 +0200

    CMOS matrix implemented

--- Lab4.py
+++ Lab4.py
@@ -9,40 +9,64 @@ from Lab2 import kernel_h1 as k1, kernel_h2 as k2, kernel_h3 as k3, kernel_h4 as
 
 if __name__ == "__main__":
 
-    # img = cv.imread("kot.jpg")
-    img = cv.imread("szlachcic1.png")
+    img = cv.imread("kot.jpg")
+    # img = cv.imread("szlachcic1.png")
+    img = cv.resize(img.copy(), (228, 228), interpolation=cv.INTER_AREA)
     input_height = img.shape[0]
     input_width = img.shape[1]
-    blank_g = np.zeros((input_height, input_width, 3), dtype=np.uint8)
-    blank_r = blank_g.copy()
-    blank_b = blank_g.copy()
-    blank = blank_g.copy()
-    for i in range(int(input_height / 2)):
-        for j in range(int(input_width / 2)):
-            blank_g[2 * i, 2 * j] = [0, img[2 * i, 2 * j][1], 0]
-            blank_g[2 * i + 1, 2 * j + 1] = [0, img[2 * i, 2 * j][1], 0]
-            blank_b[2 * i + 1, 2 * j] = [img[2 * i + 1, 2 * j][0], 0, 0]
-            blank_r[2 * i, 2 * j + 1] = [0, 0, img[2 * i, 2 * j + 1][2]]
 
-    blank = blank_g + blank_b + blank_r
-    cv.imshow("green", blank_g)
-    cv.imshow("red", blank_r)
-    cv.imshow("blue", blank_b)
-    cv.imshow("calosc", blank)
-    cv.waitKey(0)
+    Bayer = False
+    Fuji = True
+    if(Bayer is True):
+        #stworzenie podstawowej maski
+        mask_g = np.array([[1, 0], [0, 1]],dtype=np.uint8)
+        mask_b = np.array([[0, 0], [1, 0]],dtype=np.uint8)
+        mask_r = np.array([[0, 1], [0, 0]],dtype=np.uint8)
+        scale_h = input_height // 2
+        scale_w = input_width // 2
 
-    # mask_g = np.array(4, 4, 1)
-    mask_g = np.array([[1, 0], [0, 1]],dtype=np.uint8)
-    # print(mask_g)
-    scale_h = input_height // 2
-    scale_w = input_width // 2
-    tile = np.tile(mask_g, (scale_h, scale_w))
-    # tile.astype(np.uint8)
-    # print(tile)
-    green = img[:,:,1]
+    elif(Fuji is True):
+        mask_g = np.array([[1, 0, 0, 1, 0, 0], [0, 1, 1, 0, 1, 1], [0, 1, 1, 0, 1, 1], [1, 0, 0, 1, 0, 0], [0, 1, 1, 0, 1, 1], [0, 1, 1, 0, 1, 1]],dtype=np.uint8)
+        mask_b = np.array([[0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]],dtype=np.uint8)
+        mask_r = np.array([[0, 0, 1, 0, 1, 0], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0]],dtype=np.uint8)
+        scale_h = input_height // 6
+        scale_w = input_width // 6
+    
+    #powielenie podstawowej marki w celu uzyskania rozłożenia jej na całosci obrazu
+    
+    masked_g = np.tile(mask_g, (scale_h, scale_w))
+    masked_b = np.tile(mask_b, (scale_h, scale_w))
+    masked_r = np.tile(mask_r, (scale_h, scale_w))
 
-    green = green * tile
+    #wyodrębnienie przestrzeni kolorystrycznych
+    green_component = img[:, :, 1]
+    blue_component = img[:, :, 0]
+    red_component = img[:, :, 2]
+
+    if(masked_b.shape[0] != input_height):
+        diff =  input_height - masked_b.shape[0]
+        row = np.ones((diff, masked_b.shape[1]), dtype = np.uint8)
+        masked_g = np.append(masked_g, row, axis=0)
+        masked_r = np.append(masked_r, row, axis=0)
+        masked_b = np.append(masked_b, row, axis=0)
+
+    if(masked_b.shape[1] != input_width):
+        diff = input_width - masked_b.shape[1]
+        row = np.ones((masked_b.shape[0], diff), dtype = np.uint8)
+        masked_g = np.append(masked_g, row, axis=1)
+        masked_r = np.append(masked_r, row, axis=1)
+        masked_b = np.append(masked_b, row, axis=1)
+
+    green = green_component * masked_g
+    blue = blue_component * masked_b
+    red = red_component * masked_r
+
+    assembled = cv.merge((blue, green, red))
+
     cv.imshow("gren", green)
+    cv.imshow("blue", blue)
+    cv.imshow("red", red)
+    cv.imshow("assembled", assembled)
     cv.waitKey(0) 
 
 
